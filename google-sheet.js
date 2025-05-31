38787174fb5321d74a3b89f15be7d66b0790d5fa
Elena <tcachelena.gmail.com>
2025-05-31 12:23:19 +0300

    Перевод на переменные окружения вместо credentials.json

--- google-sheet.js
+++ google-sheet.js
@@ -1,40 +1,18 @@
 const { GoogleSpreadsheet } = require("google-spreadsheet");
-const creds = require("./google-credentials.json");
 
-const SHEET_ID = "1A5QKZNgSElcKpyQQY9ejaLH-GmXe_JKOg27uTFJQ2DY"; // без лишнего
+const SHEET_ID = process.env.SPREADSHEET_ID;
 
-async function addRowToSheet(row) {
-  console.log("📄 Подключаюсь к таблице...");
+const creds = {
+  client_email: process.env.EMAIL,
+  private_key: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
+};
+
+async function addRowToSheet(data) {
   const doc = new GoogleSpreadsheet(SHEET_ID);
-
-  try {
-    await doc.useServiceAccountAuth(creds);
-    console.log("✅ Авторизация прошла успешно");
-  } catch (error) {
-    console.error("❌ Ошибка авторизации:", error);
-    return;
-  }
-
-  try {
-    await doc.loadInfo();
-    console.log("📊 Загружен документ:", doc.title);
-  } catch (error) {
-    console.error("❌ Ошибка загрузки документа:", error);
-    return;
-  }
-
-  try {
-    const sheet = doc.sheetsByTitle["Покупки"];
-    if (!sheet) {
-      console.error("❌ Лист 'Покупки' не найден. Проверь название в таблице.");
-      return;
-    }
-    console.log("📄 Добавляю строку:", row);
-    await sheet.addRow(row);
-    console.log("✅ Строка успешно добавлена!");
-  } catch (error) {
-    console.error("❌ Ошибка при добавлении строки:", error);
-  }
+  await doc.useServiceAccountAuth(creds);
+  await doc.loadInfo();
+  const sheet = doc.sheetsByTitle["Покупки"];
+  await sheet.addRow(data);
 }
 
 async function getAllRows() {
@@ -46,9 +24,4 @@ async function getAllRows() {
   return rows;
 }
 
-
 module.exports = { addRowToSheet, getAllRows };
-
-
-
-
